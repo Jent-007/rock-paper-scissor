1e7af224d16f79fba7638803ecdfc776692b2c32
Jent-007 <jeansijent09@gmail.com>
2021-09-28 17:50:14 +0800

    Edited script.js

--- script.js
+++ script.js
@@ -1,79 +1,55 @@
-//
 const choices = ["Rock", "Paper", "Scissors"];
+let computerSelection;
+let playerSelection;
+let cpuScore = 0;
+let playerScore = 0;
 
 function computerPlay() {
     return choices[~~(Math.random() * choices.length)];
 }
 
-const computerSelection = computerPlay();
-const playerSelection = prompt("Rock, Paper or Scissors", '');
-let cpuScore = 0;
-let playerScore = 0;
+function showWinner() {
+    if (playerScore === 5) {
+        alert("Congratulations! You win. You defeat computer " + playerScore + " to " + cpuScore);
+    } else {
+        alert("You lose to computer " + cpuScore + " to " + playerScore);
+    }
+}
 
 function game() {
+    
     if (playerScore === 5 || cpuScore === 5) {
-        alert("Thank you for playing!")
+        showWinner();
+    } else if (playerScore === 4 || cpuScore === 4) {
+        oneRound();
+    } else if (playerScore === 3 || cpuScore === 3) {
+        oneRound();
+    } else if (playerScore === 2 || cpuScore === 2) {
+        oneRound();
+    } else if (playerScore === 1 || cpuScore === 1) {
+        oneRound();
     } else {
         oneRound();
     }
 }
 
 function oneRound(playerSelection, computerSelection) {
-
     computerSelection = computerPlay().toLowerCase();
-    playerSelection = playerSelection.toLowerCase();
-
+    playerSelection = prompt("Rock, Paper, Scissors", '').toLowerCase();
     if (playerSelection == computerSelection) {
-        alert("Tie Game!");
-    } else if ((playerSelection == 'rock' && computerSelection == 'paper') || 
-    (playerSelection == 'paper' && computerSelection == 'scissors') || 
+        console.log("Computer select " + computerSelection + ". It's a tie! Score is still " + playerScore + " for player and " + cpuScore + " for computer.");
+        oneRound();
+    } else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'rock')) {
-
-        cpuScore = ++cpuScore;
-        if (cpuScore === 1) {
-            console.log(cpuScore + " You lose. One point for Computer");
-            return prompt('');
-        } else if (cpuScore === 2) {
-            console.log(cpuScore + " Don't mind, don't mind!");
-            return playerSelection;
-        } else if (cpuScore === 3) {
-            console.log(cpuScore + " Tough luck. Three points for Computer.");
-            return playerSelection;
-        } else if (cpuScore === 4) {
-            console.log(cpuScore + " Oof another one. But there's still chance!");
-            return playerSelection;
-        } else {
-            console.log("Game over! Computer wins.");
-        }
-
+        cpuScore += 1;
+        console.log("Computer select " + computerSelection + ". Computer win. Score is " + playerScore + " for player and " + cpuScore + " for computer.");
+        game();
     } else {
-        playerScore = ++playerScore;
-        if (playerScore === 1) {
-            console.log(playerScore + " Nice. One point for you");
-            return prompt('');
-        } else if (playerScore === 2) {
-            console.log(playerScore + " Mou ikkai!");
-            return playerSelection;
-        } else if (playerScore === 3) {
-            console.log(playerScore + " Wow! You're amazing. Keep going!");
-            return playerSelection;
-        } else if (playerScore === 4) {
-            console.log(playerScore + " Almost there!");
-            return playerSelection;
-        } else {
-            console.log("Game over! You win. Congratulations!");
-            return playerSelection;
-        }
+        playerScore += 1;
+        console.log("Computer select " + computerSelection + ". Player win! Score is " + playerScore + " for player and " + cpuScore + " for computer.");
+        game();
     }
 }
 
-function scoreCpu() {
-
-} 
-
-
-oneRound(playerSelection, computerPlay());
-
-
-
-
+game();
